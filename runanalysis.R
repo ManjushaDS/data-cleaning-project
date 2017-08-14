a270ce0e96e3b8a54781ab23f93953ab8272d3b7
ManjushaDS <manjusha.etc@gmail.com>
2017-08-14 13:47:14 +0530

    Update runanalysis.R

--- runanalysis.R
+++ runanalysis.R
@@ -2,7 +2,7 @@
 # 1. Merge the training and the test sets to create one data set.
 
 ## step 1: download zip file from website
-#if(!file.exists("./data")) dir.create("./data")
+
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./getdata.zip")
 
